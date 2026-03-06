54714ffa184b3fdbe1f1c013271e6ce7a682dd82
fo1-oss <fo1@crepdogcrew.com>
2026-03-06 12:44:32 +0530

    Delete cdc-admin-deploy directory

--- cdc-admin-deploy/api/start.js
+++
@@ -1,6 +0,0 @@
-require("dotenv").config();
-const app = require("./index.js");
-const PORT = process.env.PORT || 3001;
-app.listen(PORT, () => {
-  console.log(`🚀 CDC Admin Portal API running on http://localhost:${PORT}`);
-});
